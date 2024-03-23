91b68996f12ad12feab7bfad29c654f607486cd6
Aabankhan1 <134615017+Aabankhan1@users.noreply.github.com>
2024-03-23 12:21:08 +0000

    Datatypes

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 1445;
+let accountEmail = "aabankhan1900@gmail.com";
+var accountPassword = "2245";
+accountCity = "Karachi";
+let accountStatement;
+
+// accountId = 2;
+accountEmail = "aaban@gmail.com";
+accountPassword = "25214";
+accountCity = "islamabad";
+
+console.log(accountId);
+console.table ([accountEmail, accountId, accountPassword, accountCity, accountStatement]);
+
+/*
+ prefer not to use var 
+ because of issue in block scope and functional scope 
+*/
---
+++ .devcontainer/01_Basics/02_DataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer Version
+
+// alert (3 + 3); // we are using node js not browser 
+
+// code readability should be high 
+
+let name = "Aaban";
+let age = 18;
+let loggedIn = false;
+
+//1. number => 2 to power 53
+//2. bigint 
+//3. string => ""
+//4. boolean => true/false
+//5. null => standalone value 
+//6. undefined => value is not assign
+//7. symbol 
+
+
+//objects
+
+console.log(typeof age);
+console.log (typeof null);
